485f90acd0bf940d1da6193aaad4094638124689
Ash Thompson <ashthompson06@gmail.com>
2016-09-24 12:10:31 +0100

    jenkins.sh test script

---
+++ jenkins.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PYENV_HOME=./.pyenv/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+    rm -rf $PYENV_HOME
+fi
+
+# Create virtualenv and install necessary packages
+virtualenv --no-site-packages $PYENV_HOME
+. $PYENV_HOME/bin/activate
+
+which python
+which pip
+
+pip install ez_setup bson pymongo flask passlib flask_httpauth nosexcover pylint
+pip install ./octronic/
+nosetests --with-xcoverage --with-xunit --cover-package=octronic.webapis --cover-erase
+pylint -f parseable / | tee pylint.out
